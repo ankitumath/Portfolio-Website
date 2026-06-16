a12ea06ab45849adb1c99fb8c78d5db9784ecd5d
Ankit Umath <ankit.royalhhss@gmail.com>
2026-06-16 12:42:28 +0530

    Update frontend API URL

--- frontend/src/services/contactService.js
+++ frontend/src/services/contactService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/contact";
+const API_URL = "https://portfolio-website-4sbs.onrender.com/api/contact";
 
 export const sendContactMessage = async (formData) => {
   const response = await axios.post(API_URL, formData);
